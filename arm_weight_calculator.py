ee4c797ac9ac1434fe0c01b939adf9fc239732c9
Alexandra Posta <posta_alex222@yahoo.com>
2023-11-13 12:32:01 +0000

    fix: change the dimensions

--- arm_weight_calculator.py
+++ arm_weight_calculator.py
@@ -1,12 +1,12 @@
 import math
 
 # Change this constants to test different scenarios
-drone_weight = [10., 12.]
-drone_length = 1.5
-arm_weight = [2., 5.]
-arm_length = [1.]
-tank_weight = [2.]
-tank_length = .5
+drone_weight = [15.]    # Weight of the drone in kg
+drone_length = 1.5      # Diagonal of the drone in m
+arm_weight = [6., 8.]       # Weight of the arm in kg
+arm_length = [1.1]       # Length of the arm in m
+tank_weight = [2.]      # Weight of the tank in kg
+tank_length = .5        # Length of the tank in m
 max_tilt = 20  # Max tilt angle (degrees)
 
 
@@ -58,23 +58,22 @@ def get_required_thrust(cog, arm, drone, tank):
 
 
 # Get the maximum inclination
-if __name__ == "main":
-    # Test different scenarios
-    for d_w in drone_weight:
-        for t_w in tank_weight:
-            for a_l in arm_length:
-                for a_w in arm_weight:
-                    drone = Component("drone", d_w, drone_length, drone_length/2)
-                    tank = Component("tank", t_w, tank_length, drone_length/2)
-                    arm = Component("arm", a_w, a_l, (a_l/2 + drone_length/2))
-                    components = [drone, tank, arm]
+# Test different scenarios
+for d_w in drone_weight:
+    for t_w in tank_weight:
+        for a_l in arm_length:
+            for a_w in arm_weight:
+                drone = Component("drone", d_w, drone_length, drone_length/2)
+                tank = Component("tank", t_w, tank_length, drone_length/2)
+                arm = Component("arm", a_w, a_l, (a_l/2 + drone_length/2))
+                components = [drone, tank, arm]
 
-                    # Calculate center of gravity
-                    cog = calculate_cog(components)
+                # Calculate center of gravity
+                cog = calculate_cog(components)
 
-                    if (cog > (drone_length/2 + (20/100)*drone_length/2)):
-                        print(f"The center of gravity is too far to the right. Try again. for drone weight={d_w}, tank_weight={t_w}, arm_length={a_l}, arm_weight={a_w}")
-                    else:
-                        force = get_required_thrust(cog, arm, drone, tank)    
-                        print(f"CoG={cog:.3f} for drone weight={d_w}, tank_weight={t_w}, arm_length={a_l}, arm_weight={a_w}")    
-                        print(f"Required thrust force: {force:.3f}")
+                if (cog > (drone_length/2 + (30/100)*drone_length/2)):
+                    print(f"The center of gravity is too far to the right. Try again. for drone weight={d_w}, tank_weight={t_w}, arm_length={a_l}, arm_weight={a_w}")
+                else:
+                    force = get_required_thrust(cog, arm, drone, tank)    
+                    print(f"CoG={cog:.3f} for drone weight={d_w}, tank_weight={t_w}, arm_length={a_l}, arm_weight={a_w}")    
+                    print(f"Required thrust force: {force:.3f}")
